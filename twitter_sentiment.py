c40a6b2d83473758555c0f23af6a8f9d96ebfd86
atul107 <kr.atul.atk@gmail.com>
2019-05-14 16:03:26 +0530

    Update twitter_sentiment.py

--- twitter_sentiment.py
+++ twitter_sentiment.py
@@ -10,6 +10,7 @@ from tweepy import OAuthHandler
 from tweepy import Stream
 from kafka import SimpleProducer, KafkaClient
 
+#Token Keys to access Twitter
 access_token = "1126006627360919554-O8SEeivP8x980NsoY5QhnAFr3aAd7A"
 access_token_secret =  "bCDov02FmtF9shlCTNJv0WxxlMNKsFhPYn7SlE1TlTtEm"
 consumer_key =  "Em2AcTwsqhR6XMcymOARaUej0"
