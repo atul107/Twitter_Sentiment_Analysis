a870bef75883894be7cd9c754b8827770404baa7
atul107 <kr.atul.atk@gmail.com>
2019-05-21 16:03:58 +0530

    Add files via upload

---
+++ Intern/config.py
@@ -0,0 +1,12 @@
+
+
+#Token Keys to access Twitter
+access_token = "1126006627360919554-O8SEeivP8x980NsoY5QhnAFr3aAd7A"
+access_token_secret =  "bCDov02FmtF9shlCTNJv0WxxlMNKsFhPYn7SlE1TlTtEm"
+consumer_key =  "Em2AcTwsqhR6XMcymOARaUej0"
+consumer_secret =  "wzblehM8zm3qk6t0jgRzGqvA4KOxkDt03aEB2HGxdyeGOPhxPm"
+
+producer_name = 'politics'
+index_name = 'politics'
+keywords = ['#trump', '#obama']
+
